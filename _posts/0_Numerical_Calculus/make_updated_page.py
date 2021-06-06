201c2925c301ad66e3b34f110e3ac47571f77f27
Josiel <josiel.jms7@gmail.com>
2021-06-05 23:17:43 -0300

    Python Intro Post

--- _posts/0_Numerical_Calculus/make_updated_page.py
+++ _posts/0_Numerical_Calculus/make_updated_page.py
@@ -1,21 +1,41 @@
 
 
-data0 = open('1_python_intro.html')
-lines = data0.readlines()
+data = open('1_python_intro.html')
+lines = data.readlines()
 
-new_file = open("new_page.html",'w')
+new_file = open("Tutorial_1.html",'w')
+
+header = open("../../header.html").read()
+#header = header.replace("menu_style","../../menu_style")
+header = header.replace("figures/","../../figures/")
+#header = header.replace("./index.html","../../index.html")
+header = header.replace("href=\"","href=\"../../")
+header+= "\n <section style=\" float:left; padding:20px; width: 60%;\" >\n"
+
+footer = open("../../footer.html").read()
+footer = footer.replace("menu_style","../../menu_style")
+footer = "\n </section>\n"+footer
+
+new_file.write(header)
+for line in lines: new_file.write(line)
+
+new_file.write(footer)
+
+'''
+c1, c2 = True, True
 for line in lines:
 	cond1 = line.find("<body")
 	cond2 = line.find("body>")
-	if(cond1>=0):
+	if(cond1>=0 and c1):
 		header = open("../../header.html").read()
 		header = header.replace("menu_style","../../menu_style")
 		line = header + "\n<section class=\"section_style\">\n" + line
-		print(line)
-	if(cond2>=0):
+		print(line) ; c1 = False
+	if(cond2>=0 and c2):
 		footer = open("../../footer.html").read()
 		footer = footer.replace("menu_style","../../menu_style")
 		line = "</section>\n\n" + line + footer 
-		print(line)
+		print(line) ; c2 = False
 	new_file.write(line)
 new_file.close()
+'''
